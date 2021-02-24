f8bbf645a983b9cebe41b1441726cb86df459961
adcadams <andrewdcadams@gmail.com>
2021-02-23 22:40:53 -0500

    adds entry#create

--- backend/app/controllers/entries_controller.rb
+++ backend/app/controllers/entries_controller.rb
@@ -6,5 +6,22 @@ class EntriesController < ApplicationController
         render json: entry
     end
 
+    def create
+        #binding.pry
+        if Entry.find_by(:term => entry_params[:term])
+            entry = Entry.find_by(:term => entry_params[:term])
+            redirect_to "/entries/#{entry.id}"
+        else
+            entry = Entry.create(entry_params)
+            render json: entry
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:entry).permit(:term)
+    end
+
 
 end
